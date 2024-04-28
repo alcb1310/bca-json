1b56478bfecbd1a37c754b4482112ce2402f799d
Andres Court <andres@andrescourt.com>
2024-04-27 20:07:44 -0500

    Configure a logger middleware

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 
 	"github.com/alcb1310/bca-json/internal/handlers"
 )
@@ -17,5 +18,7 @@ func NewServer() *Server {
 }
 
 func (s *Server) MountHandlers() {
+	s.Router.Use(middleware.Logger)
+
 	s.Router.Get("/foo", handlers.HandleFoo)
 }
