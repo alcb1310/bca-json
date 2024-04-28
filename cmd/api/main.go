bb51366e3c0a49811e76d8a3b361391249f057b5
Andres Court <andres@andrescourt.com>
2024-04-27 19:58:27 -0500

    Configure server to use the PORT that comes from the environment variables

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 
 	"github.com/alcb1310/bca-json/internal/server"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		slog.Error("Error loading .env file", "error", err)
+	}
+
 	s := server.NewServer()
 
 	s.MountHandlers()
-	listenAddr := ":42069"
+	listenAddr := fmt.Sprintf(":%s", os.Getenv("PORT"))
 
 	slog.Info("Starting server on", "addr", listenAddr)
 	http.ListenAndServe(listenAddr, s.Router)
