5b8273b802a46aec4c4707319c3fd8fb2ed40b55
Andres Court <andres@andrescourt.com>
2024-04-27 20:32:38 -0500

    make the foo route return a json object

--- internal/handlers/foo.go
+++ internal/handlers/foo.go
@@ -1,10 +1,15 @@
 package handlers
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func HandleFoo(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
+	response := make(map[string]interface{})
+	response["message"] = "Hello, World!"
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(response)
 }
--- internal/handlers/foo_test.go
+++ internal/handlers/foo_test.go
@@ -1,7 +1,9 @@
 package handlers_test
 
 import (
+	"encoding/json"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,10 +17,16 @@ func TestFoo(t *testing.T) {
 	s.MountHandlers()
 
 	t.Run("foo", func(t *testing.T) {
+		expected := make(map[string]interface{})
+		expected["message"] = "Hello, World!"
+		expectedByte, _ := json.Marshal(expected)
+		expectedString := string(expectedByte)
+
 		request := httptest.NewRequest("GET", "/foo", nil)
 		response := utils_test.ExecuteRequest(request, s)
 
 		assert.Equal(t, 200, response.Code)
-		assert.Equal(t, "Hello, World!", response.Body.String())
+		assert.Equal(t, "application/json", response.Header().Get("Content-Type"))
+		assert.Equal(t, expectedString, strings.Trim(response.Body.String(), "\n"))
 	})
 }
