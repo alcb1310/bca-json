1860fd25574f8c301be570b72057c45f760f482b
Andres Court <andres@andrescourt.com>
2024-04-27 22:10:58 -0500

    Added CORS

--- go.mod
+++ go.mod
@@ -7,6 +7,8 @@ require (
 	github.com/stretchr/testify v1.9.0
 )
 
+require github.com/go-chi/cors v1.2.1 // indirect
+
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/joho/godotenv v1.5.1
--- internal/server/server.go
+++ internal/server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 
 	"github.com/alcb1310/bca-json/internal/utils"
 )
@@ -19,6 +20,13 @@ func NewServer() *Server {
 
 func (s *Server) MountHandlers() {
 	s.Router.Use(middleware.Logger)
+	s.Router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	s.Router.Get("/foo", utils.Make(s.HandleFoo))
 
