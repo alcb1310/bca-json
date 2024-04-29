1de96fd5563a5450512714a25e08dc73ed8cf0b8
Andres Court <andres@andrescourt.com>
2024-04-29 18:29:20 -0500

    Password Encryption / Decryption w/tests

--- internal/utils/password_test.go
+++ internal/utils/password_test.go
@@ -12,7 +12,7 @@ import (
 func TestValidatePassword(t *testing.T) {
 	t.Run("should return error if password is empty", func(t *testing.T) {
 		password := ""
-		expectedError := errors.New("La contrasena es obligatoria")
+		expectedError := errors.New("La contraseña es obligatoria")
 
 		err := utils.ValidatePassword(password)
 
@@ -25,6 +25,42 @@ func TestValidatePassword(t *testing.T) {
 
 		err := utils.ValidatePassword(password)
 
+		assert.Equal(t, expectedError, err)
+	})
+}
+
+func TestEncryptPasssword(t *testing.T) {
+	t.Run("should encrypt password", func(t *testing.T) {
+		password := "password"
+
+		_, err := utils.EncryptPasssword(password)
+
+		assert.Nil(t, err)
+	})
+
+	t.Run("should decrypt password", func(t *testing.T) {
+		password := "password"
+
+		pass, err := utils.EncryptPasssword(password)
+
+		assert.Nil(t, err)
+
+		err = utils.ComparePassword(string(pass), password)
+
+		assert.Nil(t, err)
+	})
+
+	t.Run("should return error if incorrect password", func(t *testing.T) {
+		password := "password"
+		expectedError := errors.New("Credenciales inválidas")
+
+		pass, err := utils.EncryptPasssword(password)
+
+		assert.Nil(t, err)
+
+		err = utils.ComparePassword(string(pass), "incorrect")
+
+		assert.NotNil(t, err)
 		assert.Equal(t, expectedError, err)
 	})
 }
