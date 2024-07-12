31034e7599d12e88e40e4e01c5f93d6d18700508
Andres Court <andres@andrescourt.com>
2024-07-11 20:52:32 -0500

    testing: Set content-type for all tests

--- internals/server/common_test.go
+++ internals/server/common_test.go
@@ -5,16 +5,18 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/alcb1310/bca-json/internals/server"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/alcb1310/bca-json/internals/server"
 )
 
 func getResponse(t *testing.T, s *server.Server, r *http.Request, code int) *httptest.ResponseRecorder {
-    w := httptest.NewRecorder()
-    s.Server.ServeHTTP(w, r)
+	r.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+	s.Server.ServeHTTP(w, r)
 
-    assert.Equal(t, code, w.Code)
-    assert.Equal(t, "application/json", w.Header().Get("Content-Type"))
+	assert.Equal(t, code, w.Code)
+	assert.Equal(t, "application/json", w.Header().Get("Content-Type"))
 
-    return w
+	return w
 }
