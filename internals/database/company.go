22f82ec52287a7f0ba05f06acd1e30f69bac865f
Andres Court <andres@andrescourt.com>
2024-07-12 20:40:16 -0500

    bugfix: fix typos in sql queries

--- internals/database/company.go
+++ internals/database/company.go
@@ -15,13 +15,13 @@ func (s *service) CreateCompany(company *types.Company, user *types.CreateUser) 
     }
     defer tx.Rollback()
 
-    query := "INSERT INTO company (ruc, name, emplyees, is_active) VALUES ($1, $2, $3, $4) RETURNING id"
+    query := "INSERT INTO company (ruc, name, employees, is_active) VALUES ($1, $2, $3, $4) RETURNING id"
     if err := tx.QueryRow(query, company.Ruc, company.Name, company.Employees, company.IsActive).Scan(&company.ID); err != nil {
         slog.Error("Error creating company", "error", err)
         return err
     }
 
-    query = "INSERT INTO \"user\" (email, name, password, company_id, role_id) VALUES ($1, $2 $3, $4, $5) RETURNING id"
+    query = "INSERT INTO \"user\" (email, name, password, company_id, role_id) VALUES ($1, $2, $3, $4, $5) RETURNING id"
     if err := tx.QueryRow(query, user.Email, user.Name, user.Password, company.ID, user.RoleID).Scan(&user.ID); err != nil {
         slog.Error("Error creating user", "error", err)
         return err
